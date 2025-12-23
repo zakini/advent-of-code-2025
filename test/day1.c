837322f30f553965756282d3842b82b473c09355
Josh Wells <josh@zakini.co.uk>
2025-12-23 22:55:54 +0000

    Added CUnit and unit test for day 1
    Also reworking build system

---
+++ test/day1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <CUnit/CUnit.h>
+#include "day1.h"
+#include "../src/day1.h"
+
+void exampleTest();
+
+int day1CreateTestSuite() {
+    CU_pSuite suite = NULL;
+
+    suite = CU_add_suite("Day 1", NULL, NULL);
+    if (suite == NULL) return EXIT_FAILURE;
+
+    CU_add_test(suite, "exampleTest", exampleTest);
+
+    return EXIT_SUCCESS;
+}
+
+void exampleTest() {
+    CU_ASSERT_EQUAL(3, day1())
+}
---
+++ test/day1.h
@@ -0,0 +1,6 @@
+#ifndef DAY1_TEST_H
+#define DAY1_TEST_H
+
+int day1CreateTestSuite();
+
+#endif
